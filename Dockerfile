cf62b8bed9a898d735c1e563856903c76723ea64
steve-fischer-200 <sfischer@upenn.edu>
2026-06-16 15:57:42 -0400

    debug import

--- Dockerfile
+++ Dockerfile
@@ -23,7 +23,7 @@ RUN mkdir -p /opt/veupathdb/lib/perl \
   && unzip utils.zip \
   && rm utils.zip
 
-ARG LIB_GIT_COMMIT_SHA=768bfe1877dd8068215b0f42a9652236b77ad304
+ARG LIB_GIT_COMMIT_SHA=66d983cf6056dd12efd135673821e693fd57a0e9
 RUN git clone https://github.com/VEuPathDB/vdi-lib-plugin-rnaseq.git \
   && cd vdi-lib-plugin-rnaseq \
   && git checkout ${LIB_GIT_COMMIT_SHA} \
--- lib/perl/BigWigUtils.pm
+++ lib/perl/BigWigUtils.pm
@@ -5,7 +5,7 @@ use Exporter;
 use File::Copy;
 use File::Basename;
 our @ISA = 'Exporter';
-our @EXPORT = qw(installBwFile);
+our @EXPORT = qw(installBwFile, validateBwFile);
 
 sub installBwFile {
   my ($bwFile, $dataFilesDir) = @_;
@@ -17,5 +17,15 @@ sub installBwFile {
   chmod(0664, "$dataFilesDir/$f") or die "Could not chmod $dataFilesDir/$f\n";
 }
 
+# return invalid message or 0
+sub validateBwFile {
+  my ($filePath) = @_;
+  my $fileName = basename($filePath);
+  #  return "Invalid bigwig file: $fileName" if system("isbigwig $filePath");
+  my $sz =  -s $filePath;
+  return "Bigwig file too big (> 500M): $fileName ($sz)" if ($sz > (500 * 1024 * 1024));
+  return 0;
+}
 
 1;
+
