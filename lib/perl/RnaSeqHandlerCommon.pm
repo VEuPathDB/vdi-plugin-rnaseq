3a45a66a6acd7c4d2e78391938c4e994d39137af
steve-fischer-200 <sfischer@upenn.edu>
2026-06-19 14:20:00 -0400

    add import tests

--- lib/perl/RnaSeqHandlerCommon.pm
+++ lib/perl/RnaSeqHandlerCommon.pm
@@ -26,22 +26,51 @@ sub readAndValidateManifestLine {
 
   my @line = split(/\t/, $line);
 
-  validationError("Invalid manifest file.  Wrong number of columns: '$line'. Must be two columns, tab delimited.")
-    unless scalar(@line) == 2 || scalar(@line) == 3;  # third column for now must be 'stranded', so it is optional
+  validationError("Invalid manifest file.  Wrong number of columns. Must be tab delimited, with these columns: sample name, counts file, 'stranded'(optional), bigwig file (optional).\n '$line'")
+    unless scalar(@line) >= 2 || scalar(@line) <= 4;  # third column for now must be 'stranded', so it is optional
 
-  my ($sampleName, $filename, $strandInfo) = @line;
+  my ($sampleName, $filename, $strandInfo, $bwFile) = @line;
+
+  my $sampleName = $line[0];
+  my $bwFile;
+  my $strandInfo;
+  my $countFile;
+
+  # support legacy format where bw file is in 2nd column
+  if ($line[1] =~ /\.bw$|\.bigwig/) {
+    validationError("Invalid line in manifest file. Has more than one bigwig file. \n '$line'")
+      if ($line[2] =~ /\.bw$|\.bigwig/ || $line[3] =~ /\.bw$|\.bigwig/);
+    validationError("Invalid line in manifest file. If bigwig file in second column, fourth column not allowed.\n '$line'")
+      if $line[3];
+    $bwFile = $line[1];
+    $strandInfo = $line[2];
+  }
+  # strand column omitted
+  elsif ($line[2] =~ /\.bw$|\.bigwig/){
+    $countFile = $line[1];
+    $bwFile = $line[2];
+  } else {
+    $countFile = $line[1];
+    $strandInfo = $line[2];
+    validationError("Invalid line in manifest file.  Fourth column must be .bw or .bigwig file\n '$line'")
+      if ($line[3] && $line[3] !~ /\.bw$|\.bigwig/);
+    $bwFile = $line[3];
+  }
 
   $strandInfo = 'unstranded' unless $strandInfo;
 
-  validationError("Invalid manifest file third column value '$strandInfo'.  Must be: " . join(', ', @VALID_STRAND_TYPES))
+  validationError("Invalid line in manifest file.  Strandedness must be: " . join(', ', @VALID_STRAND_TYPES) . "\n '$line'")
       unless grep( /^$strandInfo$/, @VALID_STRAND_TYPES );
-  my $path = "$dataFilesDir/$filename";
 
-  print STDERR "sample: $sampleName filepath: $path strand: $strandInfo\n";
+  print STDERR "sample: '$sampleName' countFile: '$countFile' strand: '$strandInfo' bwFile: '$bwFile' \n";
 
-  validationError("File in manifest does not exist: '$path'") unless -e $path;
+  my $path = "$dataFilesDir/$countFile";
+  validationError("Counts file in manifest does not exist: '$countFile'")
+    unless !$countFile || -e "$dataFilesDir/$countFile";
+  validationError("Bigwig file in manifest does not exist: '$bwFile'")
+    unless !$bwFile || -e "$dataFilesDir/$bwFile";
 
-  return @line;
+  return ($sampleName, $countFile, $strandInfo, $bwFile);
 }
 
 sub validationError {
